1b8cd0b030309a7bd4bcf5c13ba370a8f1b49296
AgustinSt1990 <jackone.action.software@gmail.com>
2022-11-02 20:05:08 -0300

    Quedo obsoleto - subpaquete creado

--- apps/forex_api.py
+++ apps/forex_api.py
@@ -1,11 +1,22 @@
+""""
+ESTE ARCHIVO ESTA TOTALMENTE OBSOLETO, HA SIDO DIVIDO EN UN SUBPAQUETE
+
+LA RAZON DE CONSERVARLO ES POR LAS PRUEBAS QUE SE EJECUTAN EN EL __name__ == '__main__'
+
+CON MOTIVO DE USARSE COMO MATERIAL PARA LA DOCUMENTACION
+
+"""
+
+
 """https://www.alphavantage.co/documentation/"""
-#TODO: finnhub documentation
+# TODO: finnhub documentation
 
 import os
 from dotenv import load_dotenv  # remember: pip install python-dotenv
 import requests
 import pandas as pd
 from datetime import datetime
+import datetime as dt
 
 
 def fx_price(function, from_symbol, to_symbol, api_key, outputsize="full"):
@@ -16,13 +27,19 @@ def fx_price(function, from_symbol, to_symbol, api_key, outputsize="full"):
     from_symbol: first asset
     to_symbol: second asset
     api_key: personal TOKEN
-    outputsize: "full", "compact", 
+    outputsize: "full", "compact",
     Return: pandas.DataFrame
     """
 
     URL_BASE = "https://www.alphavantage.co/query"
 
-    parametros = {'function': function, 'from_symbol': from_symbol, 'to_symbol':to_symbol,'outputsize':outputsize, 'apikey': api_key}
+    parametros = {
+        "function": function,
+        "from_symbol": from_symbol,
+        "to_symbol": to_symbol,
+        "outputsize": outputsize,
+        "apikey": api_key,
+    }
 
     r = requests.get(URL_BASE, params=parametros)
     data = r.json()
@@ -42,62 +59,93 @@ def fx_price(function, from_symbol, to_symbol, api_key, outputsize="full"):
     return data
 
 
-
-
 def fx_it(function, symbol, interval, apikey=None, **kargs):
     URL_BASE = "https://www.alphavantage.co/query"
-    parametros = {'function':function, 'symbol':symbol, 'interval':interval,
-    **kargs,
-    'apikey':apikey
+    parametros = {
+        "function": function,
+        "symbol": symbol,
+        "interval": interval,
+        **kargs,
+        "apikey": apikey,
     }
 
     r = requests.get(URL_BASE, params=parametros)
     try:
         js = r.json()[f"Technical Analysis: {function}"]
-        df = pd.DataFrame.from_dict(js, orient='index')
+        df = pd.DataFrame.from_dict(js, orient="index")
         df = df.astype(float)
         df.index.name = "Date"
-        df = df.sort_values('Date', ascending=True).round(3)
+        df = df.sort_values("Date", ascending=True).round(3)
         df.index = pd.to_datetime(df.index)
     except Exception as e:
-        print (f"Exception: {e}")
+        print(f"Exception: {e}")
         df = pd.DataFrame()
-        print (r.json())
+        print(r.json())
     except:
         df = pd.DataFrame()
-        print (r.json())
+        print(r.json())
     return df
-
 
 
 def fx_get_exchanges(TOKEN):
     url = "https://finnhub.io/api/v1/forex/exchange"
-    r = requests.get(url, params={'token': TOKEN})
+    r = requests.get(url, params={"token": TOKEN})
     js = r.json()
     return js
 
+
 def fx_exchange_symbols(exchange, TOKEN):
     url = "https://finnhub.io/api/v1/forex/symbol"
-    p = {'token': TOKEN, 'exchange': exchange}
+    p = {"token": TOKEN, "exchange": exchange}
     r = requests.get(url, params=p)
     js = r.json()
     return js
 
-# def fx_symbol_candles(exchange, symbol, interval, fromS, toS, TOKEN):
-# NECESITA SERVICIO PREMIUN PARA USAR EL ENDPOINT
-#     s = exchange + ':' + symbol
-#     fromDT = datetime.strptime(fromS, '%Y-%m-%d')
-#     fromTS = int(datetime.timestamp(fromDT))
-#     toDT = datetime.strptime(toS, '%Y-%m-%d')
-#     toTS = int(datetime.timestamp(toDT))
 
-#     url = "https://finnhub.io/api/v1/forex/candle"
-#     p = {'token':TOKEN, 'symbol': s, 'resolution':interval, 'from':fromTS, 'to':toTS}
-#     r = requests.get(url, params=p)
-#     js = r.json()
+# TODO: Parametros por default
+# https://polygon.io/docs/forex/getting-started
+def fx_symbol_candles(forexTicker, multiplier, timespan, fromS, toS, apikey):
+    """
+    timespan's: minute, hour, day, week, month, quarter, year
+    fromS, toS: need format %Y-%m-%d
+    """
+    function = "/aggs"
+    default_params = "adjusted=true&sort=asc&limit=50000"
 
-#     # df = pd.DataFrame(js)
-#     return js
+    URL_BASE = f"https://api.polygon.io/v2{function}"
+
+    URL_BASE += f"/ticker/C:{forexTicker}/range/{multiplier}/{timespan}/{fromS}/{toS}"
+    URL_BASE += f"?{default_params}&apiKey={apikey}"
+
+    r = requests.get(URL_BASE)
+    js = r.json()
+
+    df = pd.DataFrame(js["results"])
+    try:
+        df["date"] = pd.to_datetime(df["t"], unit="ms")
+    except:
+        df["date"] = pd.to_datetime(df["t"])
+    df.set_index("date", inplace=True)
+    df.drop(columns="t", inplace=True)
+    df.rename(
+        columns=dict(
+            zip(
+                df.columns,
+                ["Volume", "VolWeiAvgPrc", "Open", "Close", "High", "Low", "Qtx"],
+            )
+        ),
+        inplace=True,
+    )
+    df = df.reindex(
+        ["Close", "High", "Low", "Open", "Volume", "Qtx", "VolWeiAvgPrc"], axis=1
+    )
+    if sum(df["Volume"] - df["Qtx"]) == 0:
+        df.drop(columns="Qtx", inplace=True)
+    return df
+
+
+def convert_to_timestamp(dateDT: str) -> datetime.timestamp:
+    return int(dt.datetime.timestamp(dateDT)) * 1000
 
 
 if __name__ == "__main__":
@@ -111,9 +159,16 @@ if __name__ == "__main__":
 
     TOKEN2 = os.environ["TOKEN_FH"]
     # pprint(fx_get_exchanges(TOKEN2))
-    """['oanda', 'fxcm','forex.com', 'icmtrader', 'fxpro', 'pepperstoneuk', 'ic markets', 'fxpig', 'pepperstone']""";
+    """['oanda', 'fxcm','forex.com', 'icmtrader', 'fxpro', 'pepperstoneuk', 'ic markets', 'fxpig', 'pepperstone']"""
 
-    # pprint (fx_exchange_symbols('fxpro', TOKEN2)) 
+    # pprint (fx_exchange_symbols('fxpro', TOKEN2))
     # OK
 
-    pprint (fx_symbol_candles('fxpro', 'NZD/USD', 60, '2022-01-01', '2022-05-05',TOKEN2))
+    #### pprint (fx_symbol_candles('fxpro', 'NZD/USD', 60, '2022-01-01', '2022-05-05',TOKEN2))
+
+    TOKEN3 = os.environ["TOKEN_PG"]
+    pprint(
+        len(
+            fx_symbol_candles("EURJPY", "1", "hour", "2018-01-01", "2022-10-29", TOKEN3)
+        )
+    )
